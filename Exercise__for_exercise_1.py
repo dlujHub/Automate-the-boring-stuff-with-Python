13c8b2a178a1cda1ea1a9f6de871ef9b38d96bab
dlujHub <dlujanschi2@gmail.com>
2019-03-09 17:55:18 +0200

    stuff

---
+++ Exercise__for_exercise_1.py
@@ -0,0 +1,21 @@
+# Exercise: for exercise 1
+
+# (5/5 points)
+# ESTIMATED TIME TO COMPLETE: 5 minutes
+
+# In this problem you'll be given a chance to practice writing some for loops.
+
+# 1. Convert the following code into code that uses a for loop.
+
+# prints 2
+# prints 4
+# prints 6
+# prints 8
+# prints 10
+# prints "Goodbye!"
+
+for i in range(2, 11, 2):
+    print(i)
+print('Goodbye!')
+
+# Correct
